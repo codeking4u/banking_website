d7c9c5cabcafc312b0d6bd2909ce7d8f289c1060
Amar <codeking4u@gmail.com>
2021-02-01 01:11:28 +0530

    hamburger animations added

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-console.log("HEY JS");
+const headerMenu = document.querySelector(".header__menu");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay")
+
+headerMenu.addEventListener("click", function(){
+    header.classList.toggle("open");
+    if( header.classList.contains('open')){
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    }else{
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+    }
+});
